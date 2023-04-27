ec1d439deff7330d4474c94fe7e332c36a6a4757
hesamsrk <hesamsrk@gmail.com>
2023-04-27 20:16:06 +0330

    codebase replaced

--- client/app.config.ts
+++ client/app.config.ts
@@ -1,49 +1,49 @@
-import {ConfigContext, ExpoConfig} from 'expo/config';
-import {config} from "dotenv"
-config()
+import {ExpoConfig} from 'expo/config';
+import * as dotenv from "dotenv"
+
+dotenv.config()
 const env = Object.fromEntries(Object.entries(process.env).filter(([key, value]) => key.startsWith("CW_")))
 console.log({env})
 
-export default ({config}: ConfigContext): ExpoConfig => ({
-        ...config,
-        primaryColor:"#1C2541",
-        assetBundlePatterns: [
-            'assets/**/*',
-        ],
-        name: "crypto-wallet",
-        slug: "crypto-wallet",
-        version: "1.0.0",
-        orientation: "portrait",
-        icon: "./assets/icon.png",
-        userInterfaceStyle: "light",
-        splash: {
-            image: "./assets/splash.png",
-            resizeMode: "contain",
-            backgroundColor: "#ffffff"
+const config: ExpoConfig = {
+    name: 'crypto-wallet',
+    slug: 'crypto-wallet',
+    version: "1.0.0",
+    primaryColor: "#1C2541",
+    orientation: "portrait",
+    assetBundlePatterns: [
+        "assets/**/*"
+    ],
+    icon: "./assets/icon.png",
+    splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+    },
+    userInterfaceStyle: "light",
+    extra: {
+        eas: {
+            projectId: "f03908de-08bb-4f0f-93b3-e16c69ce9fae",
+            env
+        }
+    },
+    owner: "hesamsrk",
+    updates: {
+        fallbackToCacheTimeout: 5000
+    },
+    ios: {
+        supportsTablet: false,
+        infoPlist: {
+            NSFaceIDUsageDescription: "This app uses FaceID or TouchID to verify users"
+        }
+    },
+    android: {
+        adaptiveIcon: {
+            foregroundImage: "./assets/icon.png",
+            backgroundColor: "#FFFFFF"
         },
-        updates: {
-            fallbackToCacheTimeout: 5000
-        },
-        ios: {
-            supportsTablet: false,
-            infoPlist: {
-                NSFaceIDUsageDescription: "This app uses FaceID or TouchID to verify users"
-            }
-        },
-        android: {
-            adaptiveIcon: {
-                foregroundImage: "./assets/icon.png",
-                backgroundColor: "#FFFFFF"
-            },
-            package: "com.hesamsrk.cryptowallet"
-        },
-        extra: {
-            eas: {
-                projectId: "f03908de-08bb-4f0f-93b3-e16c69ce9fae",
-                env
-            }
-        },
-        owner: "hesamsrk"
+        package: "com.hesamsrk.cryptowallet"
+    },
+};
 
-    }
-);
+export default config;
